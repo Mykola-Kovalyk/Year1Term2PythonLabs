2368476cf69378f3e56e337229c595bc944ad643
NicolasIceberg <93177243+MykolaKovalyk@users.noreply.github.com>
2022-06-08 18:42:16 +0300

    Corrections made so that all the given task conditions are met

--- lab11/__init__.py
+++ lab11/__init__.py
@@ -1,33 +1,41 @@
 import re
 import datetime
+import zipfile
 
 
-def find_by_date(file_path, date_begin, date_end):
+def find_by_date(archive_path, file_path, date_begin, date_end):
 
     return_list = []
-    with open(file_path, 'r') as f:
-        lines = f.readlines()
-        for i in range(len(lines)):
-            line = lines[i]
-            try:
-                occurence = re.search(r"\[[^\]]+.", line)
-                if occurence:
-                    date_as_str = occurence.group()[1:-7]
-                    date = datetime.datetime.strptime(date_as_str, '%d/%b/%Y:%H:%M:%S')
 
-                    if date_begin <= date <= date_end:
-                        return_list.append(line)
-                    elif date > date_end:
-                        break
-            except Exception as e:
-                print(f'An error occured while parsing line {i} ("{line}"): {e}')
+    with zipfile.ZipFile(archive_path, 'r') as archive:
+        with archive.open(file_path, 'r') as file:
+            lines = file.readlines()
+            for i in range(len(lines)):
+                line = lines[i].decode()
+                try:
+                    if re.search('NASA', line):
+                        occurence = re.search(r"\[[^\]]+.", line)
+                        if occurence:
+                            date_as_str = occurence.group()[1:-7]
+                            date = datetime.datetime.strptime(date_as_str, '%d/%b/%Y:%H:%M:%S')
+
+                            if date_begin <= date <= date_end:
+                                return_list.append(line)
+                            elif date > date_end:
+                                break
+                except Exception as e:
+                    print(f'An error occurred while parsing line {i} ("{line}"): {e}')
 
     return return_list
 
 
 
 if __name__ == '__main__':
-    print(find_by_date('access_log_Jul95',
-                       datetime.datetime(1995, 7, 1, 0, 10),
-                       datetime.datetime(1995, 7, 1, 0, 20)))
+
+    count = len(find_by_date('access_log_Jul95.zip',
+                             'access_log_Jul95',
+                       datetime.datetime(1995, 7, 1, 0, 7),
+                       datetime.datetime(1995, 7, 1, 2, 21)))
+
+    print(f"There's {count} occurrences found.")
 
